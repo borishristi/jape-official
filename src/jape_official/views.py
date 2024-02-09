c8b1b50cc041fae10b39d251302f20e5e55d43f0
Hristi <borisarroga@gmail.com>
2024-02-09 15:47:05 +0100

    I create the index.html file who will be the home page of the website

--- src/jape_official/views.py
+++ src/jape_official/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+from datetime import datetime
+
+from django.shortcuts import render
 
 
 def index_view(request):
-    return HttpResponse("<h1>Hello World</h2>")
+    date = datetime.today()
+    print(date)
+    print(type(date))
+    return render(request, 'index.html', context={"first_name": "Hristi", "date": date})
