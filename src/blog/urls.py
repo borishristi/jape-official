48260227355f55739a5b27abb1ef13b51e67dee1
Hristi <borisarroga@gmail.com>
2024-02-13 14:48:15 +0100

    I modify the articles sections grid.

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,4 +1,7 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from jape_official import settings
 from .views import index_view, article_view, template_view
 
 
@@ -6,4 +9,4 @@ urlpatterns = [
     # path('', index_view, name='blog-index_view'),
     path('article-<str:numero_article>/', article_view, name='blog-article'),
     path('', template_view, name='template_view')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
